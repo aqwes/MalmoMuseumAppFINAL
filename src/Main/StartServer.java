3da4c159c770264c3d9e8bdd157590f58e88b2db
Dennis <aqwees@gmail.com>
2016-03-09 13:32:51 +0100

    pom

--- src/Main/StartServer.java
+++ src/Main/StartServer.java
@@ -6,14 +6,7 @@ import Server.Server;
 
 class StartServer {
     public static void main(String[] args) {
-        Server socketServer = new Server(getHerokuAssignedPort());
+        Server socketServer = new Server(9990());
     }
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; // return default port if heroku-port isn't set (i.e. on
-        // localhost)
     }
 }
